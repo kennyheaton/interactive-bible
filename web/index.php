47efb3182bdc594241984777535cc6bcf826ca93
Kenny Heaton <kennyheaton@icloud.com>
2020-12-05 15:21:42 -0600

    Initial commit

---
+++ web/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en-US">
+ <head>
+  <meta charset="utf-8">
+  <title>Interactive Bible</title>
+ </head>
+ <body>
+  <?php echo'<p>Hello World</p>';?>
+ </body>
+</html>
